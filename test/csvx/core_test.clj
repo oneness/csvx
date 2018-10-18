2def3aaa26f21e87448bd7411405a99a391dbbcf
Kasim Tuman <ktuman@gmail.com>
2018-10-18 12:29:16 -0700

    Added json test

--- test/csvx/core_test.clj
+++ test/csvx/core_test.clj
@@ -24,3 +24,43 @@
             [{0 "John"} {1 "32"} {2 "M"}]
             [{0 "Susan"} {1 "28"} {2 "F"}]]
            (-> (sample-csv) readx)))))
+
+(defn sample-json []
+  (tmp-file "json-sample" ".json"
+            "{\"user_id\":\"lzlZwIpuSWXEnNS91wxjHw\",\"name\":\"Susan\",\"review_count\":1,\"yelping_since\":\"2015-09-28\",\"friends\":\"None\",\"useful\":0,\"funny\":0,\"cool\":0,\"fans\":0,\"elite\":\"None\",\"average_stars\":2.0,\"compliment_hot\":0,\"compliment_more\":0,\"compliment_profile\":0,\"compliment_cute\":0,\"compliment_list\":0,\"compliment_note\":0,\"compliment_plain\":0,\"compliment_cool\":0,\"compliment_funny\":0,\"compliment_writer\":0,\"compliment_photos\":0}"))
+
+(deftest parse-json-test
+  (testing "Given a json file where each line is valid json, parse it into [{}]"
+    (is (= [{:elite "None",
+            :compliment_cute 0,
+            :fans 0,
+            :cool 0,
+            :compliment_hot 0,
+            :compliment_plain 0,
+            :compliment_profile 0,
+            :funny 0,
+            :name "Susan",
+            :compliment_more 0,
+            :yelping_since "2015-09-28",
+            :compliment_list 0,
+            :compliment_cool 0,
+            :compliment_writer 0,
+            :friends "None",
+            :useful 0,
+            :average_stars 2.0,
+            :user_id "lzlZwIpuSWXEnNS91wxjHw",
+            :compliment_funny 0,
+            :compliment_note 0,
+            :review_count 1,
+            :compliment_photos 0}]
+           (-> (sample-json)
+               (readx {:line-tokenizer (fn [line]
+                                         (map #(.split ^String % ":")
+                                              (-> (clojure.string/replace line #"\{|\}" "")
+                                                  (.split ","))))
+                       :line-transformer (fn [line]
+                                           (reduce (fn [acc [k v]]
+                                                     (merge acc
+                                                            {(-> k read-string keyword) (read-string v)}))
+                                                   {}
+                                                   line))}))))))
