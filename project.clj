6c2d1802db6107cd161a4ba812655062206c349b
Kasim Tuman <ktuman@gmail.com>
2024-09-26 11:57:41 -0700

    Made few updates, namely:
    * Replaced project.clj with deps.edn
    * Updated clojure version to 1.12.0
    * Updated README with git clone and test commands

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject co.birkey/csvx "0.1.0-SNAPSHOT"
-  :description "A dependency-free char(s) separated values parser/transformer that puts you in control."
-  :url "https://www.birkey.co"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :profiles {:dev {:dependencies [[citius "0.2.4"]]}})
